4e0004e19ef933f6106412cf5a540b834a48875e
nikini888 <luutuyen37k8@gmail.com>
2022-04-25 23:05:04 +0700

    Add style

---
+++ app.js
@@ -0,0 +1,33 @@
+
+const GameBoard = function () {
+    const gameBoard = [
+        {
+            play: 'player1',
+            move: 2,
+        },
+        {
+            play: 'player2',
+            move: 4
+        }
+    ]
+
+
+    return {}
+}
+const displayController = (function () {
+
+})()
+const players = function (name) {
+    const getType = () => {
+        document.querySelector
+    }
+    getName(name)
+    return { name, score }
+}
+document.querySelectorAll('.cell').forEach((cell, item) => {
+    cell.addEventListener('click', (e) => {
+        e.target.firstElementChild.textContent = 'X'
+        const preMove = item;
+    })
+}
+)
